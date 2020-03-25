1e2dad3ab22bb8512d4c0c966544e9ab61f837b8
Adrian Badarau <adi.badarau@gmail.com>
2020-03-25 21:21:17 +0100

    changed the transaction service so we can query for all transactions for an account

--- src/main/kotlin/com/adrianbadarau/bank/transactions/repository/TransactionRepository.kt
+++ src/main/kotlin/com/adrianbadarau/bank/transactions/repository/TransactionRepository.kt
@@ -1,6 +1,8 @@
 package com.adrianbadarau.bank.transactions.repository
 
 import com.adrianbadarau.bank.transactions.domain.Transaction
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
@@ -9,4 +11,6 @@ import org.springframework.stereotype.Repository
  */
 @Suppress("unused")
 @Repository
-interface TransactionRepository : JpaRepository<Transaction, Long>
+interface TransactionRepository : JpaRepository<Transaction, Long> {
+    fun findAllByAccountIdEquals(accountId: String, pageable: Pageable):Page<Transaction>
+}
